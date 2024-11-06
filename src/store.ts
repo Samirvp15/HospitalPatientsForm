60258508de8a3d3ef79ba3b2ac4fe5afffae3462
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-06 16:57:21 -0500

    add zustand and initial config

---
+++ src/store.ts
@@ -0,0 +1,12 @@
+
+import { create } from 'zustand';
+import { Patient } from './types';
+
+
+type PatientState = {
+    patients: Patient[]
+}
+
+export const usePatientStore = create<PatientState>(() => ({
+    patients: []
+}))
