28f975166038cd655a85ad6fc14a9ea402375f9f
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-06 18:03:15 -0500

    add Redux Devtools

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,3 +1,4 @@
+import { usePatientStore } from "../store"
 import { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
 
@@ -9,6 +10,10 @@ type PatientDetailsProps = {
 }
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
+
+
+  const { deletePatient, getPatientById } = usePatientStore()
+
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
 
@@ -36,6 +41,23 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
         label="Sintomas"
         data={patient.symptoms}
       />
+
+      <div className="flex flex-col gap-3 lg:flex-row justify-between mt-10">
+        <button className=" py-2 px-10 bg-indigo-600 text-white
+       font-bold uppercase rounded-lg"
+          onClick={() => getPatientById(patient.id)}
+        >
+          Editar
+        </button>
+        <button className=" py-2 px-10 bg-red-600 text-white
+       font-bold uppercase rounded-lg"
+          onClick={() => deletePatient(patient.id)}
+        >
+          Eliminar
+        </button>
+      </div>
+
+
     </div>
   )
 }
