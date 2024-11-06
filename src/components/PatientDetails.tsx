fa364939b566ae894089d649c24968166ba7d483
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-06 17:29:55 -0500

    add PatientDetails and customComponent with PatientStore

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,41 @@
+import { Patient } from "../types"
+import PatientDetailItem from "./PatientDetailItem"
+
+
+
+
+type PatientDetailsProps = {
+  patient: Patient
+}
+
+export default function PatientDetails({ patient }: PatientDetailsProps) {
+  return (
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+
+      <PatientDetailItem
+        label="ID"
+        data={patient.id}
+      />
+      <PatientDetailItem
+        label="Nombre"
+        data={patient.name}
+      />
+      <PatientDetailItem
+        label="Propietario"
+        data={patient.caretaker}
+      />
+      <PatientDetailItem
+        label="Correo"
+        data={patient.email}
+      />
+      <PatientDetailItem
+        label="Fecha de Alta"
+        data={patient.date.toString()}
+      />
+      <PatientDetailItem
+        label="Sintomas"
+        data={patient.symptoms}
+      />
+    </div>
+  )
+}
